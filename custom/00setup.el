10054490b1062f694da4a87fd318b889f96d0eb0
Hemant Kumar <gethemant@gmail.com>
2009-12-25 00:59:45 +0530

    update setup directory

--- custom/00setup.el
+++ custom/00setup.el
@@ -77,3 +77,4 @@
 (setq uniquify-separator "/")
 (setq uniquify-after-kill-buffer-p t) ; rename after killing uniquified
 (setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers
+
