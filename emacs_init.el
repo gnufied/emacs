2867e348d0421ae077cfecb4f677947841191de6
Hemant Kumar <gethemant@gmail.com>
2009-12-25 01:10:46 +0530

    check in ruby-mode

--- emacs_init.el
+++ emacs_init.el
@@ -1,5 +1,11 @@
-(setq load-path
-      (append (list nil "~/new_emacs"
-		    "~/new_emacs/custom"
-		    "~/new_emacs/org-mode/lisp")))
+(add-to-list 'load-path "~/new_emacs")
+(add-to-list 'load-path	"~/new_emacs/custom")
+(add-to-list 'load-path "~/new_emacs/org-mode/lisp")
+(add-to-list 'load-path "~/new_emacs/ruby-mode")
+
 (load "00setup.el")
+(load "01org-mode.el")
+(load "02ruby-mode.el")
+;;(load "03rails-mode.el")
+;;(load "04scala-mode.el")
+
