1ad8feb519f6925d18be2232e1678821120e5a9c
Hemant Kumar <hkumar@crri.co.in>
2011-01-13 01:28:56 +0530

    js2 mode for javascript editing

--- emacs_init.el
+++ emacs_init.el
@@ -43,6 +43,9 @@
 (require 'feature-mode)
 (add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
 
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
 ;;(load "17rsense.el")
 
 (server-start)
