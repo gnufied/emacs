ec460b4bee9a5f6e5ba010db249aef344319fa3c
Hemant Kumar <gethemant@gmail.com>
2011-09-06 10:21:55 +0530

    Comment out some lines

--- custom/27emacsclient.el
+++ custom/27emacsclient.el
@@ -1,7 +1,7 @@
-(add-hook 'server-switch-hook
-	  (lambda nil
-	    (let ((server-buf (current-buffer)))
-	      (bury-buffer)
-	      (switch-to-buffer-other-frame server-buf))))
-(add-hook 'server-done-hook 'delete-frame)
+;; (add-hook 'server-switch-hook
+;; 	  (lambda nil
+;; 	    (let ((server-buf (current-buffer)))
+;; 	      (bury-buffer)
+;; 	      (switch-to-buffer-other-frame server-buf))))
+;; (add-hook 'server-done-hook 'delete-frame)
 (add-hook 'server-done-hook (lambda nil (kill-buffer nil)))
