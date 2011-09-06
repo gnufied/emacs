8a2cc7a5c9b25093e77e0427f96e0ae4faf9e111
Hemant Kumar <gethemant@gmail.com>
2011-09-06 10:24:48 +0530

    Add hook to remove menu-bar from emacs terminal window

--- custom/27emacsclient.el
+++ custom/27emacsclient.el
@@ -4,4 +4,9 @@
 ;; 	      (bury-buffer)
 ;; 	      (switch-to-buffer-other-frame server-buf))))
 ;; (add-hook 'server-done-hook 'delete-frame)
+
+(add-hook 'server-switch-hook
+	  (lambda ()
+	    (menu-bar-mode)))
+
 (add-hook 'server-done-hook (lambda nil (kill-buffer nil)))
